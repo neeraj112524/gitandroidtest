5ac2f3582fb56307895c28c16adb26200aa08806
Neeraj Gyawali <helloneeraj@gmail.com>
2018-12-12 20:36:27 +0545

    Initial commit

---
+++ app/src/main/java/com/example/neeraj/gitandroidtest/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.neeraj.gitandroidtest
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
